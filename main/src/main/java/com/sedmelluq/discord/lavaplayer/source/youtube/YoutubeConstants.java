8021f7e69c8f675fa389fc3d6988367d514f95c0
SomboyTiger <160553347+licha-oreshi@users.noreply.github.com>
2024-02-20 07:48:33 +0700

    fix error status code 400

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeConstants.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeConstants.java
@@ -9,7 +9,7 @@ public class YoutubeConstants {
 
     static final String INNERTUBE_ANDROID_API_KEY = "AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w";
     static final String CLIENT_ANDROID_NAME = "ANDROID";
-    static final String CLIENT_ANDROID_VERSION = "17.29.34";
+    static final String CLIENT_ANDROID_VERSION = "18.06.35";
 
     static final String INNERTUBE_WEB_API_KEY = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8";
     static final String CLIENT_WEB_NAME = "WEB";
@@ -35,7 +35,7 @@ public class YoutubeConstants {
 
     static final String INNERTUBE_MUSIC_API_KEY = "AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30";
     static final String CLIENT_MUSIC_NAME = "WEB_REMIX";
-    static final String CLIENT_MUSIC_VERSION = "0.1";
+    static final String CLIENT_MUSIC_VERSION = "1.20220727.01.00";
 
     static final String SEARCH_MUSIC_PARAMS = "Eg-KAQwIARAAGAAgACgAMABqChADEAQQCRAFEAo=";
 
